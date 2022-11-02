7a470970696b8aeced6cacf37997dcd4418eb6f8
Hamraz Ahmad <thamrazeeba123@gmail.com>
2022-11-02 18:19:00 +0530

    check the code

---
+++ Prime_Problem.js
@@ -0,0 +1,16 @@
+function checkPrime(num){
+let count=0;
+ for(let i=i; i<=num; i++){
+    if(num%i){
+      count++;
+
+}
+  return count;
+}
+
+let ans=checkPrime(13);
+if(count==2){
+  console.log("Prime Number");
+}else{
+  console.log("Not a Prime Number");
+}
